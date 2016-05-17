5790c42e8a79921691a96509f1f37f094410fc00
Host <Host@HOST-PC>
2016-05-17 20:40:11 +0900

    Main_Form_Design

--- Base_project/Form1.Designer.cs
+++ Base_project/Form1.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbox_id = new System.Windows.Forms.TextBox();
+            this.tbox_pw = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_reg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(14, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "기초 프로젝트 커뮤니티!!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "아이디 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "패스워드 :";
+            // 
+            // tbox_id
+            // 
+            this.tbox_id.Location = new System.Drawing.Point(91, 90);
+            this.tbox_id.Name = "tbox_id";
+            this.tbox_id.Size = new System.Drawing.Size(230, 21);
+            this.tbox_id.TabIndex = 3;
+            // 
+            // tbox_pw
+            // 
+            this.tbox_pw.Location = new System.Drawing.Point(91, 139);
+            this.tbox_pw.Name = "tbox_pw";
+            this.tbox_pw.Size = new System.Drawing.Size(230, 21);
+            this.tbox_pw.TabIndex = 4;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(214, 185);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(107, 34);
+            this.btn_login.TabIndex = 5;
+            this.btn_login.Text = "로그인";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // btn_reg
+            // 
+            this.btn_reg.Location = new System.Drawing.Point(91, 185);
+            this.btn_reg.Name = "btn_reg";
+            this.btn_reg.Size = new System.Drawing.Size(107, 34);
+            this.btn_reg.TabIndex = 6;
+            this.btn_reg.Text = "회원가입";
+            this.btn_reg.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(358, 266);
+            this.Controls.Add(this.btn_reg);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tbox_pw);
+            this.Controls.Add(this.tbox_id);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "기초프로젝트 커뮤니티";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbox_id;
+        private System.Windows.Forms.TextBox tbox_pw;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_reg;
     }
 }
 
